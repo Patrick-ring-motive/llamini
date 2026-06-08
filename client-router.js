2447573f8dcbae83bae3a4ffce38688c8fa42e47
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-08 11:14:53 -0500

    Update client-router.js

--- client-router.js
+++ client-router.js
@@ -40,7 +40,7 @@
                     const url = String(args[0].url ?? args[0]);
                     if (routes[url]) {
                         const routesURL = String(routes[url].url ?? routes[url]);
-                        const res = await _fetch(routesURL);
+                        const res = await _fetch(`${routesURL}?${new Date().getTime()}`);
                         if (routes[url].headers) {
                             const value = new Headers(res.headers.entries());
                             for (header in routes[url].headers) {
