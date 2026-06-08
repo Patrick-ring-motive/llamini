ed963804fbf462ef9f45d89c0faca5bddd965e48
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-08 05:47:10 -0500

    style: js-beautify formatting (coi-serviceworker.js)

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,30 +1,27 @@
-
-
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-const corsHeader = {'access-control-allow-origin':"*"};
+const corsHeader = {
+  'access-control-allow-origin': "*"
+};
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
 
- 
-    const pro = (async()=>{
-      const url = new URL(e.request.url);
-      url.searchParams.set('headers',JSON.stringify(corsHeader));
-      return new Request(String(url),e.request);
-    })().then(req)=>fetch(req)).then(res => {
-      if (!res || res.status === 0 || res.type === 'opaque') return res;
+  const pro = (async () => {
+    const url = new URL(e.request.url);
+    url.searchParams.set('headers', JSON.stringify(corsHeader));
+    return new Request(String(url), e.request);
+  })().then(req) => fetch(req)).then(res => {
+  if (!res || res.status === 0 || res.type === 'opaque') return res;
 
-      const headers = new Headers(res.headers);
-      headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-      headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  const headers = new Headers(res.headers);
+  headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+  headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
 
-      return new Response(res.body, {
-        status: res.status,
-        statusText: res.statusText,
-        headers,
-      });
-    }).catch(() => fetch(e.request)).catch((e)=>new Response(String(e));
-  e.waitUntil(pro);
-  e.respondWith(pro);
+  return new Response(res.body, {
+    status: res.status,
+    statusText: res.statusText,
+    headers,
+  });
+}).catch(() => fetch(e.request)).catch((e) => new Response(String(e)); e.waitUntil(pro); e.respondWith(pro);
 });
