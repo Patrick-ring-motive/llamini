2ee6fada5d3f02f5f7b63e48f7d8d799edda3e29
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-08 10:50:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 (async()=>{
-  await import('./client-router.js');
-  await import('./llamini.js');
+  await import('./client-router.js?'+new Date().getTIme());
+  await import('./llamini.js?'+new Date().getTIme());
 })();
