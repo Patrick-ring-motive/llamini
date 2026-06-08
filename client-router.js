4d97ea4ed2dd1652a619181d825fa0456224c50b
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-08 09:59:39 -0500

    Update client-router.js

--- client-router.js
+++ client-router.js
@@ -1,40 +1,70 @@
-    (()=>{
-      const routes = {
-        'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.5.0/dist/ort-wasm-simd-threaded.jsep.wasm':{
-          url: './ort-wasm-simd-threaded.jsep.gz',
-          headers : {
-            'content-type':'application/wasm'
-          }
-        },
-        'https://huggingface.co/Xenova/LaMini-T5-61M/resolve/main/config.json':'./llamini-config.json',
-        'https://huggingface.co/Xenova/LaMini-T5-61M/resolve/main/tokenizer.json':'./llamini-tokenizer.json',
-        'https://huggingface.co/Xenova/LaMini-T5-61M/resolve/main/tokenizer_config.json':'./llamini-tokenizer-config.json'
-      };
-      const _fetch = globalThis.fetch;
-      globalThis.fetch = Object.setPrototypeOf(async function fetch(...args){
-        try{
-            console.log(...args);
-          const url = String(args[0].url ?? args[0]);
-          if(routes[url]){
-            const routesURL = routes[url].url ?? routes[url];
-            const res = await _fetch(routesURL);
-            if(routes[url].headers){
-              const value = new Headers(res.headers.entries());
-              for(header in routes[url].headers){
-                value.set(key,routes[url].headers[key]);
-              }
-              Object.defineProptery(res,'headers',{value});
-            }
-            if(routesURL.endsWith('.gz')){
-                const ds = new DecompressionStream("gzip");
-                return new Response(res.body.pipeThrough(new DecompressionStream("gzip")),res);
-            }
-            return res;
-          }
-          return await _fetch.apply(this,args);
-        }catch(e){
-          const statusText = String(e);
-          return new Response(statusText,{staus:500,statusText});
-        }
-      },_fetch);
+    (() => {
+        const routes = {
+            'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.5.0/dist/ort-wasm-simd-threaded.jsep.wasm': {
+                url: './ort-wasm-simd-threaded.jsep.gz',
+                headers: {
+                    'content-type': 'application/wasm'
+                }
+            },
+            'https://huggingface.co/Xenova/LaMini-T5-61M/resolve/main/config.json': './llamini-config.json',
+            'https://huggingface.co/Xenova/LaMini-T5-61M/resolve/main/tokenizer.json': './llamini-tokenizer.json',
+            'https://huggingface.co/Xenova/LaMini-T5-61M/resolve/main/tokenizer_config.json': './llamini-tokenizer-config.json'
+        };
+        (()=>{
+            const _parse = JSON.parse;
+            JSON.parse = Object.setPrototypeOf(function parse(...args){
+                try{
+                    return _parse.apply(this,args);
+                }catch(e){
+                    console.warn(e,...args);
+                    throw e;
+                }
+            },_parse);
+        })();
+        (()=>{
+            const _json = Response.prototype.json;
+            Response.prototype.json = Object.setPrototypeOf(function json(...args){
+                try{
+                    return await _json.apply(this,args);
+                }catch(e){
+                    console.warn(e,...args);
+                    throw e;
+                }
+            },_json);
+        })();
+        (() => {
+            const _fetch = globalThis.fetch;
+            globalThis.fetch = Object.setPrototypeOf(async function fetch(...args) {
+                try {
+                    console.log(...args);
+                    const url = String(args[0].url ?? args[0]);
+                    if (routes[url]) {
+                        const routesURL = routes[url].url ?? routes[url];
+                        const res = await _fetch(routesURL);
+                        if (routes[url].headers) {
+                            const value = new Headers(res.headers.entries());
+                            for (header in routes[url].headers) {
+                                value.set(key, routes[url].headers[key]);
+                            }
+                            Object.defineProptery(res, 'headers', {
+                                value
+                            });
+                        }
+                        if (routesURL.endsWith('.gz')) {
+                            const ds = new DecompressionStream("gzip");
+                            return new Response(res.body.pipeThrough(new DecompressionStream("gzip")), res);
+                        }
+                        return res;
+                    }
+                    return await _fetch.apply(this, args);
+                } catch (e) {
+                    console.warn(e, ...args);
+                    const statusText = String(e);
+                    return new Response(statusText, {
+                        staus: 500,
+                        statusText
+                    });
+                }
+            }, _fetch);
+        })();
     })();
