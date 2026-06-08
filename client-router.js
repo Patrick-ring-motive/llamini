eb382e9e1636dfdadd263f8cdb2f03f49b490605
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-08 11:22:38 -0500

    Refactor fetch function to handle responses correctly

--- client-router.js
+++ client-router.js
@@ -35,12 +35,13 @@
         (() => {
             const _fetch = globalThis.fetch;
             globalThis.fetch = Object.setPrototypeOf(async function fetch(...args) {
+                let res;
                 try {
                     console.log(...args);
                     const url = String(args[0].url ?? args[0]);
                     if (routes[url]) {
                         const routesURL = String(routes[url].url ?? routes[url]);
-                        const res = await _fetch(`${routesURL}?${new Date().getTime()}`);
+                        res = await _fetch(`${routesURL}?${new Date().getTime()}`);
                         if (routes[url].headers) {
                             const value = new Headers(res.headers.entries());
                             for (header in routes[url].headers) {
@@ -52,13 +53,16 @@
                         }
                         if (routesURL.endsWith('.gz')) {
                             const ds = new DecompressionStream("gzip");
-                            return new Response(res.body.pipeThrough(new DecompressionStream("gzip")), res);
+                            res = new Response(res.body.pipeThrough(new DecompressionStream("gzip")), res);
                         }
+                        console.log(res,await res.clone().text());
                         return res;
                     }
-                    return await _fetch.apply(this, args);
+                    res = await _fetch.apply(this, args);
+                    console.log(res,await res.clone().text());
+                    return res;
                 } catch (e) {
-                    console.warn(e, ...args);
+                    console.warn(e,res, ...args);
                     const statusText = String(e);
                     return new Response(statusText, {
                         staus: 500,
