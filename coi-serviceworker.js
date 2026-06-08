3b1af23cd0cf626d3085d0f5b21ac4e47effd477
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-07 20:35:22 -0500

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -2,13 +2,17 @@
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-
+const corsHeader = {'access-control-allow-origin':"*"};
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
 
  
-    const pro = fetch(e.request).then(res => {
+    const pro = (async()=>{
+      const url = new URL(e.request.url);
+      url.searchParams.set('headers',JSON.stringify(corsHeader));
+      return new Request(String(url),e.request);
+    })().then(req)=>fetch(req)).then(res => {
       if (!res || res.status === 0 || res.type === 'opaque') return res;
 
       const headers = new Headers(res.headers);
