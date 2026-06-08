97e5fd242af5c6e0583c7685052e7ec44419defd
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-08 11:06:19 -0500

    Fix typo in getTime method call

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 (async()=>{
-  await import('./client-router.js?'+new Date().getTIme());
-  await import('./llamini.js?'+new Date().getTIme());
+  await import('./client-router.js?'+new Date().getTime());
+  await import('./llamini.js?'+new Date().getTime());
 })();
