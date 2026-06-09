1fad9ac45ef7728b96fff2474688c20a4d4bfe76
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-09 12:33:19 -0500

    Create decompress-stream.js

---
+++ decompress-stream.js
@@ -0,0 +1,50 @@
+let compressionFormat;
+let pakoReady;
+
+const pakoInit = async()=>{
+  await import('https://cdn.jsdelivr.net/npm/pretty-pako/pretty-pako.js');
+};
+
+const pakoInflate = async(record) =>{
+  if(!pakoReady){
+    pakoReady = pakoInit();
+  }
+  if(pakoReady intanceof Promise){
+    pakoReady = await pakoReady;
+  }
+  if(!record)return;
+  const stream = record.clone().body;
+  const inflator = new pako.Inflate();
+  const decoder = new TextDecoder();
+  for await (const chunk of stream) {
+    inflator.push(chunk);
+  }
+  const output = inflator.result;
+  return output;
+};
+
+const canDecompressFormat = format =>{
+  try{
+    new DecompressionStream(format);
+  }catch{
+    return false;
+  }
+};
+
+const br = canDecompressFormat('brotli') && 'brotli';
+const gz = canDecompressFormat('gzip') && 'gzip';
+
+compressionFormat = br || gz || 'pako';
+
+if(compressionFormat === 'pako'){
+  pakoInflate();
+}
+
+const decompressStream = (record,format) =>{
+  const destream = new DecompressionStream(format);
+  return record.clone().body.pipeThrough(destream);
+};
+
+const decompress = record =>{
+  
+};
