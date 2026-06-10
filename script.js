0082fff6bb260c2accee9362d47c65e59eaa1bba
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-10 02:20:58 -0500

    style: js-beautify formatting (script.js)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-(async()=>{
+(async () => {
   const env = /dev/i.test(location.href) ? 'DEV' : 'PROD';
-  await import('./client-router.js?'+(env == 'DEV' ? new Date().getTime() : ''));
-  await import('./llamini.js?'+(env == 'DEV' ? new Date().getTime() : ''));
+  await import('./client-router.js?' + (env == 'DEV' ? new Date().getTime() : ''));
+  await import('./llamini.js?' + (env == 'DEV' ? new Date().getTime() : ''));
 })();
